836248e9946499ce2d8dba193c1fcc5f2eee7160
Dhaval572 <dp9718776@gmail.com>
2024-11-07 17:46:37 +0530

    Update GuessTheNumber.cpp

--- GuessTheNumber.cpp
+++ GuessTheNumber.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-#include <ctime>
+#include <ctime> // For use srand()
 using namespace std;
 
 int main()
 {
 	int noOfGuess = 0;
 	srand(time(0));
-	int randNum = (rand() % 100) + 1;// generates random number
+	int randNum = (rand() % 100) + 1; // generates random number
 	int guessed;
 	cout << "Guess the number(Between 1 to 100): ";
 	do
 	{
 		cin >> guessed;
 		noOfGuess++;
-		if (guessed > randNum)
+		if (guessed > randNum) // Checking that number is bigger than random number
 		{
 			cout << "Enter lower number: \n";
 		}
-		else if (guessed < randNum)
+		else if (guessed < randNum) // Checking that number is smaller than random number
 		{
 			cout << "Enter higher number: \n";
 		}
